f0f76946415d1b1a8a2dee1f4509c66c747aa5b7
Topper Bowers <topper@toppingdesign.com>
2022-08-11 14:52:15 +0200

    add the skale token abis

---
+++ contracts/skale-token/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@dethcrypto/eth-sdk'
+
+export default defineConfig({
+  contracts: {
+    mainnet: {
+      skl: '0x00c83aeCC790e8a4453e5dD3B0B4b3680501a7A7',
+    },
+  },
+})
