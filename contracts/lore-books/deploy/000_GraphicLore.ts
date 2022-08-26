6b6c3f5bec09355ab99a589e53d73737aa280829
Topper Bowers <topper@toppingdesign.com>
2022-08-26 17:08:33 +0200

    the contract for the graphclore contract

---
+++ contracts/lore-books/deploy/000_GraphicLore.ts
@@ -0,0 +1,23 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import testnetAddrs from '../delphs-table/deployments/skaletest/addresses.json'
+import mainnetAddrs from '../delphs-table/deployments/skale/addresses.json'
+import { network } from "hardhat";
+
+const func: DeployFunction = async function ({
+  deployments,
+  getNamedAccounts,
+}: HardhatRuntimeEnvironment) {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const trustedForwarderAddress = network.tags['test'] ? testnetAddrs.TrustedForwarder : mainnetAddrs.TrustedForwarder
+
+  await deploy("GraphicLore", {
+    from: deployer,
+    gasLimit: 4000000,
+    log: true,
+    args: [trustedForwarderAddress, deployer],
+  });
+};
+export default func;
--- contracts/lore-books/hardhat.config.ts
+++ contracts/lore-books/hardhat.config.ts
@@ -1,8 +1,42 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+    minter: {
+      default: 1, // here this will by default take the first account as deployer
+    },
+  },
+  networks: {
+    skale: {
+      url: "https://mainnet.skalenodes.com/v1/haunting-devoted-deneb",
+      accounts: [
+        process.env.MAINNET_DEPLOYER_PRIVATE_KEY,
+        process.env.DELPHS_PRIVATE_KEY,
+      ].filter(
+        (k) => !!k
+      ) as string[],
+      tags: ["mainnet"],
+    },
+    skaletest: {
+      url: "https://staging-v2.skalenodes.com/v1/rapping-zuben-elakrab",
+      // gasPrice: 0,
+      accounts: [
+        process.env.SKALE_TEST_PRIVATE_KEY,
+        process.env.DELPHS_PRIVATE_KEY,
+      ].filter((k) => !!k) as string[],
+      tags: ["test", "testskale"],
+    },
+  }
 };
 
 export default config;
