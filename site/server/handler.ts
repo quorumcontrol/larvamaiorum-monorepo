16d1490f047c9dcd3e014773254a7f2505c38586
Topper Bowers <topper@toppingdesign.com>
2022-09-07 14:41:13 +0200

    shuffle the bot names

--- site/server/handler.ts
+++ site/server/handler.ts
@@ -42,8 +42,22 @@ function hashString(msg: string) {
 
 const botSetup = isTestnet ? testnetBots : mainnetBots
 
+function shuffle(array:any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 async function getBots(num: number) {
   const botNames = Object.keys(botSetup)
+  shuffle(botNames)
   return botNames.slice(0, num).map((name) => {
     return {
       name,
