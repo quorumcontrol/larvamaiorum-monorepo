6dd0ec6252e5ff4e27ea38654e36a55b3348b8a5
Topper Bowers <topper@toppingdesign.com>
2022-10-07 14:34:35 +0200

    keep getting that logic wrong

--- site/src/utils/networks.ts
+++ site/src/utils/networks.ts
@@ -1,7 +1,7 @@
 import testnetAddresses from '../../contracts/deployments/skaletest/addresses.json'
 import mainnetAddresses from '../../contracts/deployments/skale/addresses.json'
 
-export const isTestnet = !process.env.NEXT_PUBLIC_MAINNET || (process.env.NEXT_PUBLIC_CONTEXT == "branch-deploy")
+export const isTestnet = (process.env.NEXT_PUBLIC_CONTEXT == "branch-deploy") || !process.env.NEXT_PUBLIC_MAINNET
 
 export const addresses = () => {
   if (isTestnet) {
