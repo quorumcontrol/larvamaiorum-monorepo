22d3447a8372bae5a258195d106049c46e8a5146
Topper Bowers <topper@toppingdesign.com>
2022-10-07 14:38:25 +0200

    wtf

--- site/src/utils/networks.ts
+++ site/src/utils/networks.ts
@@ -1,7 +1,9 @@
 import testnetAddresses from '../../contracts/deployments/skaletest/addresses.json'
 import mainnetAddresses from '../../contracts/deployments/skale/addresses.json'
 
-export const isTestnet = (process.env.NEXT_PUBLIC_CONTEXT == "branch-deploy") || !process.env.NEXT_PUBLIC_MAINNET
+console.log("NEXT_PUBLIC_CONTEXT: ", process.env.NEXT_PUBLIC_CONTEXT, process.env.NEXT_PUBLIC_MAINNET)
+export const isTestnet = (process.env.NEXT_PUBLIC_CONTEXT === "branch-deploy") || !process.env.NEXT_PUBLIC_MAINNET
+console.log("is testnet: ", isTestnet)
 
 export const addresses = () => {
   if (isTestnet) {
