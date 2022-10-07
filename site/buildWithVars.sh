67124f0e68216d723aceee9f2e3a7aac2559a9ab
Topper Bowers <topper@toppingdesign.com>
2022-10-07 14:39:34 +0200

    export the var

--- site/buildWithVars.sh
+++ site/buildWithVars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # This makes the netlify CONTEXT available to the front-end
-NEXT_PUBLIC_CONTEXT=${CONTEXT};
+export NEXT_PUBLIC_CONTEXT=${CONTEXT};
 echo "Build Context: ${NEXT_PUBLIC_CONTEXT}";
 next build;
