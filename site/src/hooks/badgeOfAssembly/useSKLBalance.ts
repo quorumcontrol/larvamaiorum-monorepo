9d7d6494a45953ef23ed148afe2e9d453c8aaa3e
Topper Bowers <topper@toppingdesign.com>
2022-10-21 18:59:26 +0200

    login changes to hopefully fix, but hard to test because works locally both ways

--- site/src/hooks/badgeOfAssembly/useSKLBalance.ts
+++ site/src/hooks/badgeOfAssembly/useSKLBalance.ts
@@ -2,19 +2,22 @@ import { BigNumber } from "ethers";
 import { useQuery } from "react-query";
 import mainnetProvider from "../../utils/mainnetProvider";
 import { Skl__factory } from "../../../skale-token-contracts";
+import { memoize } from "../../utils/memoize";
 
 export const SKL_ADDRESS = "0x00c83aeCC790e8a4453e5dD3B0B4b3680501a7A7"
 
-const skl = Skl__factory.connect(SKL_ADDRESS, mainnetProvider);
+const skl = memoize(() => {
+  return  Skl__factory.connect(SKL_ADDRESS, mainnetProvider());
+})
 
 const useSKLBalance = (address?: string) => {
   const fetchBalance = async () => {
     const [balance, staked] = await Promise.all([
-      mainnetProvider.send("alchemy_getTokenBalances", [
+      mainnetProvider().send("alchemy_getTokenBalances", [
         address,
         [SKL_ADDRESS],
       ]),
-      skl.callStatic.getAndUpdateDelegatedAmount(address!),
+      skl().callStatic.getAndUpdateDelegatedAmount(address!),
     ]);
     const liquid = BigNumber.from(balance.tokenBalances[0].tokenBalance);
     console.log('liquid: ', liquid)
