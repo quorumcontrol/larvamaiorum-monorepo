aa55959663d69b83d8eb5e657340225ac5b67d17
Topper Bowers <topper@toppingdesign.com>
2022-10-28 12:53:42 +0200

    2 column

--- site/pages/masks/index.tsx
+++ site/pages/masks/index.tsx
@@ -29,14 +29,47 @@ const MaskIndex: NextPage = () => {
       </Head>
       <Layout>
         <VStack w="full" spacing="10">
-          <Flex w="100%" maxH="450px" alignItems="center" bgColor="black">
+          <Flex
+            w="100%"
+            backgroundImage={["none", border]}
+            borderBottom={["1px dashed", "none"]}
+            borderBottomColor={"brand.orange"}
+            alignItems="center"
+            p={boxPadding}
+            pb="50px"
+          >
+            <Stack
+              direction={["column", "row"]}
+              spacing="10"
+              w="100%"
+              alignItems="center"
+            >
+              <Box>
+              <Video
+                animationUrl="/video/masksOfTheAncientsPromo.mp4"
+                controls
+                loop
+                maxW="600px"
+              />
+              </Box>
+              <Spacer />
+              <VStack maxW="30em" alignItems="left">
+                <Heading size={["xl", "2xl"]}>Masks of the Ancients</Heading>
+                <Text>
+                  Wootgump infused masks allow the wearer to join the summoning ritual and bring forth an artifact or warrior to the Crypto Roman world.
+                </Text>
+              </VStack>
+            </Stack>
+          </Flex>
+
+          {/* <Flex w="100%" maxH="450px" alignItems="center" bgColor="black">
               <Video
                 animationUrl="/video/masksOfTheAncientsPromo.mp4"
                 controls
                 loop
                 maxH="450px"
               />
-          </Flex>
+          </Flex> */}
 
           <Flex
             w="100%"
@@ -56,10 +89,12 @@ const MaskIndex: NextPage = () => {
               <VStack maxW="22em" alignItems="left">
                 <Heading size={["xl", "2xl"]}>Where?</Heading>
                 <Text>
-                  Active on Delph&apos;s Table? Pre-mint masks for $GUMP 15,000 starting today.
+                  Active on Delph&apos;s Table? Pre-mint masks for $GUMP 15,000
+                  starting today.
                 </Text>
                 <Text>
-                  Purchase the masks in other currencies as soon as NFTrade launches on SKALE (soon).
+                  Purchase the masks in other currencies as soon as NFTrade
+                  launches on SKALE (soon).
                 </Text>
                 <Box>
                   <Link href="/masks/reserve">
@@ -72,7 +107,14 @@ const MaskIndex: NextPage = () => {
 
               <Spacer />
               <Box>
-                <Video animationUrl="/video/maskRapidFire.mp4" autoPlay loop muted playsInline maxH="450px" />
+                <Video
+                  animationUrl="/video/maskRapidFire.mp4"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  maxH="450px"
+                />
               </Box>
             </Stack>
           </Flex>
@@ -95,21 +137,28 @@ const MaskIndex: NextPage = () => {
               <VStack maxW="22em" alignItems="left">
                 <Heading size={["xl", "2xl"]}>Summon</Heading>
                 <Text>
-                  Masks of the Ancients are minted with three rarities: Uncommon, Rare, and Ultra-Rare.
+                  Masks of the Ancients are minted with three rarities:
+                  Uncommon, Rare, and Ultra-Rare.
                 </Text>
                 <Text>
-                  Wear your mask to the summoning ritual to bring forth a warrior or artifact of equal rarity. Each mask may only be used once, but the art and PFP is yours forever.
+                  Wear your mask to the summoning ritual to bring forth a
+                  warrior or artifact of equal rarity. Each mask may only be
+                  used once, but the art and PFP is yours forever.
                 </Text>
               </VStack>
 
               <Spacer />
               <Box mt="10">
-                <Video animationUrl="/video/2-masks-rotating.mp4" loop muted autoPlay playsInline />
+                <Video
+                  animationUrl="/video/2-masks-rotating.mp4"
+                  loop
+                  muted
+                  autoPlay
+                  playsInline
+                />
               </Box>
             </Stack>
           </Flex>
-
-          
         </VStack>
       </Layout>
     </>
