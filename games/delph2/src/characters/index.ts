7d2c1fcc6b5e382772d985ab4cec4b3d80a57a76
Topper Bowers <topper@toppingdesign.com>
2022-11-12 22:58:37 +0100

    working with lots of gump

---
+++ games/delph2/src/characters/index.ts
@@ -0,0 +1,2 @@
+export * from './PlayerController'
+export * from './NonPlayerCharacter'
---
+++ games/delph2/src/game/PlayingField.ts
@@ -0,0 +1,35 @@
+import { BoundingBox, Vec3 } from "playcanvas";
+import { ScriptTypeBase } from "../types/ScriptTypeBase";
+import { createScript } from "../utils/createScriptDecorator";
+import { randomBounded } from "../utils/randoms";
+
+@createScript("playingField")
+class PlayingField extends ScriptTypeBase {
+  boundingBox: BoundingBox
+  
+  initialize() {
+    var aabb = new pc.BoundingBox();
+    this.entity.render!.meshInstances.forEach((mi, i) => {
+        if (i === 0)
+            aabb.copy(mi.aabb);
+        else
+            aabb.add(mi.aabb);
+    });
+
+    this.boundingBox = aabb
+  }
+
+  randomPosition(): Vec3 {
+    const tileBoundingBox = this.boundingBox
+    let xSize = tileBoundingBox.halfExtents.x;
+    let zSize = tileBoundingBox.halfExtents.z;
+    return new Vec3(
+      this.entity.getPosition().x + randomBounded(xSize / 2),
+      0,
+      this.entity.getPosition().z + randomBounded(zSize / 2)
+    )
+  }
+
+}
+
+export default PlayingField;
