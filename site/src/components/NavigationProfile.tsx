7b44058f0d3a44eac7d78a9a172ef92ca58a2a6b
Topper Bowers <topper@toppingdesign.com>
2023-01-04 15:19:24 +0100

    better window closing and error handling

--- site/src/components/NavigationProfile.tsx
+++ site/src/components/NavigationProfile.tsx
@@ -10,12 +10,13 @@ import {
   ModalBody,
   ModalContent,
   ModalOverlay,
+  Spinner,
   Text,
   VStack,
 } from "@chakra-ui/react"
 
 import { ConnectButton } from "@rainbow-me/rainbowkit"
-import { chain, useAccount, useConnect, useDisconnect } from "wagmi"
+import { useAccount, useConnect } from "wagmi"
 import NextLink from "next/link"
 import { useUsername } from "../hooks/Player"
 import useIsClientSide from "../hooks/useIsClientSide"
@@ -31,7 +32,8 @@ import web3auth from "../utils/web3auth"
 
 const NavigationProfile: React.FC = () => {
   const [showModal, setShowModal] = useState(false)
-  const { disconnect } = useDisconnect()
+  const [connecting, setConnecting] = useState(false)
+  const [errors, setErrors] = useState("")
   const { connect, connectors } = useConnect()
   const { address, isConnected } = useAccount()
   const { data: username } = useUsername(address)
@@ -44,14 +46,84 @@ const NavigationProfile: React.FC = () => {
   }
 
   const onSocialLoginClick = async (loginType: string) => {
-    console.log("connect")
-    await web3auth.connectTo(loginType)
-    console.log("connected")
-    console.log("connectors: ", connectors)
-    connect({
-      connector: connectors.find((c) => c.id === "web3Auth"),
-    })
-    setShowModal(false)
+    try {
+      setErrors("")
+      console.log("connect")
+      setConnecting(true)
+      await web3auth.connectTo(loginType)
+      console.log("connected")
+      setShowModal(false)
+      connect({
+        connector: connectors.find((c) => c.id === "web3Auth"),
+      })
+    } catch (err) {
+      console.error("error connecting: ", err)
+      setErrors("Something went wrong")
+    } finally {
+      setConnecting(false)
+    }
+
+  }
+
+  const LoginModalContent = () => {
+    if (connecting) {
+      return (
+        <VStack alignItems="left" spacing="6">
+          <Text>Complete sign in in the popup window.</Text>
+          <Spinner />
+        </VStack>
+      )
+    }
+    return (
+      <>
+        {errors && (
+          <Text color="red">{errors}</Text>
+        )}
+        <VStack alignItems="left" spacing="6">
+          <DiscordLoginButton onClick={() => onSocialLoginClick("discord")} />
+          <Text>Or use</Text>
+          <HStack spacing="12">
+            <LinkBox
+              onClick={() => onSocialLoginClick("google")}
+              cursor="pointer"
+            >
+              <VStack alignItems="center">
+                <Icon as={FcGoogle} boxSize="12" />
+                <Text>Google</Text>
+              </VStack>
+            </LinkBox>
+            <LinkBox
+              onClick={() => onSocialLoginClick("twitter")}
+              cursor="pointer"
+            >
+              <VStack>
+                <SocialIcon network="twitter" />
+                <Text>Twitter</Text>
+              </VStack>
+            </LinkBox>
+            <LinkBox
+              onClick={() => onSocialLoginClick("apple")}
+              cursor="pointer"
+            >
+              <VStack>
+                <Icon as={FaApple} boxSize="12" color={"white"} />
+                <Text>Apple</Text>
+              </VStack>
+            </LinkBox>
+          </HStack>
+        </VStack>
+        <Heading size="lg" my="8" mt="12">
+          Have a crypto wallet?
+        </Heading>
+        <Button as={"div"} variant="primary">
+          <ConnectButton
+            showBalance={false}
+            chainStatus={"none"}
+            accountStatus="avatar"
+          />
+        </Button>
+      </>
+    )
   }
 
   if (!isClient || !username || !isConnected) {
@@ -62,42 +134,7 @@ const NavigationProfile: React.FC = () => {
           <ModalContent p="6" bg="brand.background" maxW="600px">
             <ModalBody backgroundImage={border} p="6">
               <Heading size="lg">Sign up or Sign in</Heading>
-              <VStack alignItems="left" spacing="6">
-                <DiscordLoginButton
-                  onClick={() => onSocialLoginClick("discord")}
-                />
-                <Text>Or use</Text>
-                <HStack spacing="12">
-                  <LinkBox onClick={() => onSocialLoginClick("google")} cursor="pointer">
-                    <VStack alignItems="center">
-                      <Icon as={FcGoogle} boxSize="12" />
-                      <Text>Google</Text>
-                    </VStack>
-                  </LinkBox>
-                  <LinkBox onClick={() => onSocialLoginClick("twitter")} cursor="pointer">
-                    <VStack>
-                      <SocialIcon network="twitter" />
-                      <Text>Twitter</Text>
-                    </VStack>
-                  </LinkBox>
-                  <LinkBox onClick={() => onSocialLoginClick("apple")} cursor="pointer">
-                    <VStack>
-                      <Icon as={FaApple} boxSize="12" color={"white"} />
-                      <Text>Apple</Text>
-                    </VStack>
-                  </LinkBox>
-                </HStack>
-              </VStack>
-              <Heading size="lg" my="8" mt="12">
-                Have a crypto wallet?
-              </Heading>
-              <Button as={"div"} variant="primary">
-                <ConnectButton
-                  showBalance={false}
-                  chainStatus={"none"}
-                  accountStatus="avatar"
-                />
-              </Button>
+              <LoginModalContent />
             </ModalBody>
           </ModalContent>
         </Modal>
