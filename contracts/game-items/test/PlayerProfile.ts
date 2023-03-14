18a0500ff30ce8d0fee1515b81a5628fde98e159
Topper Bowers <topper@toppingdesign.com>
2023-03-14 13:25:19 +0100

    add a small test for player profile

--- contracts/game-items/test/PlayerProfile.ts
+++ contracts/game-items/test/PlayerProfile.ts
@@ -1,7 +1,25 @@
 import { expect } from "chai"
-import { ethers } from "hardhat"
+import { deployments, ethers } from "hardhat"
+import { PlayerProfile__factory } from "../typechain-types"
 
-describe("PlayerProfile", () => {
+describe.only("PlayerProfile", () => {
+    const setupTest = deployments.createFixture(
+        async ({ deployments, ethers }) => {
+            await deployments.fixture(); // ensure you start from a fresh deployments
+
+            const signers = await ethers.getSigners()
+            const deployer = signers[0]
+            const deploys = await deployments.all()
+
+            const playerProfile = PlayerProfile__factory.connect(deploys.PlayerProfile.address, deployer)
+
+            return {
+                signers,
+                deployer,
+                playerProfile,
+            }
+        }
+    )
 
     it("deploys a small enough contract", async () => {
         const deployer = (await ethers.getSigners())[0]
@@ -9,6 +27,12 @@ describe("PlayerProfile", () => {
         expect(Buffer.from(fact.bytecode.slice(2), "hex").byteLength).to.be.lessThan(24576)
         const player = await fact.deploy(deployer.address)
         await player.deployed()
+    })
+
+    it("can get the balance of a user", async () => {
+        const { playerProfile, deployer } = await setupTest()
+
+        expect(await playerProfile.balanceOf(deployer.address)).to.equal(0)
     })
 
 })
