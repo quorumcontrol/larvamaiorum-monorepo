5c37ce66f0a3b839fc708a44375b5c6e8e1cccde
Topper Bowers <topper@toppingdesign.com>
2023-03-30 11:11:24 +0200

    add pm2

--- games/pickle-chess-server/script/testParams.ts
+++ games/pickle-chess-server/script/testParams.ts
@@ -5,6 +5,12 @@ const game = {
   numberOfHumans: 2,
 }
 
+console.log("---")
+
+console.log("---- Local Graphics / server AI dev ---- ")
+console.log("player tobowers: ", `https://launch.playcanvas.com/1653026?arena=true&debug=true&ministats=true&m=${Buffer.from(JSON.stringify({...game, numberOfHumans: 1, numberOfAi: 1, id: "0x123", name: "tobowers", avatar: "https://api.readyplayer.me/v1/avatars/63a18daedf6a9ef0482c8579.glb"})).toString("base64url")}`)
+
+
 console.log("----dev----")
 console.log("player tobowers: ", `https://launch.playcanvas.com/1653026?debug=true&ministats=true&m=${Buffer.from(JSON.stringify({...game, id: "0x123", name: "tobowers", avatar: "https://api.readyplayer.me/v1/avatars/63a18daedf6a9ef0482c8579.glb"})).toString("base64url")}`)
 console.log("player nifab: ", `https://launch.playcanvas.com/1653026?debug=true&ministats=true&m=${Buffer.from(JSON.stringify({...game, id: "0xabc", name: "nifab", avatar: "https://api.readyplayer.me/v1/avatars/63b2a4248596abea61093458.glb"})).toString("base64url")}`)
