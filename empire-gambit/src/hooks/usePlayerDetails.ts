67b833bd5b5f181a397fc79afd5cd8296a464e31
Topper Bowers <topper@toppingdesign.com>
2023-04-04 22:06:12 +0200

    appwrite for the game page

---
+++ empire-gambit/src/hooks/usePlayerDetails.ts
@@ -0,0 +1,128 @@
+import { Databases, ID, Models, Query } from "appwrite"
+import { useQuery } from "react-query";
+import client from "../utils/appwrite";
+
+const DATABASE_ID = "642ae0350a2d4ab93cad"
+const PLAYER_COLLECTION = "642be791ddb0b0c5c29d"
+const LEVEL_MATCH_COLLECTION = "642c04490913ec6c43a6"
+
+const MAX_LEVELING_GAMES_PER_DAY = 4
+
+type PlayerDoc = Models.Document & {
+  address: string
+  wins: number
+  levelup_wins: number
+}
+
+type MatchLevelDoc = Models.Document & {
+  player: string,
+  winner: boolean,
+}
+
+const playsToday = async (player: string): Promise<[number, number]> => {
+  const databases = new Databases(client);
+  try {
+    const today = new Date(new Date().toUTCString())
+    today.setHours(0, 0, 0, 0)
+    const matches = await databases.listDocuments<MatchLevelDoc>(
+      DATABASE_ID,
+      LEVEL_MATCH_COLLECTION,
+      [
+        Query.equal("player", player),
+        Query.greaterThan("$createdAt", today.toISOString()),
+        Query.limit(0),
+      ]
+    )
+    const wins = await databases.listDocuments<MatchLevelDoc>(
+      DATABASE_ID,
+      LEVEL_MATCH_COLLECTION,
+      [
+        Query.greaterThan("$createdAt", today.toISOString()),
+        Query.equal("player", player),
+        Query.equal("winner", true),
+        Query.limit(0),
+      ]
+    )
+    console.log("matches: ", matches)
+    return [matches.total, wins.total]
+  } catch (err) {
+    console.error("err: ", err)
+    throw err
+  }
+}
+
+export interface PlayerDetails {
+  level: number,
+  nextLevelIn: number,
+  wins: number,
+  levelupWins: number,
+  todaysGames: number,
+  todaysWins: number,
+  maxPerDay: number,
+}
+
+const playerDetail = async (playerAddress: string): Promise<PlayerDetails|undefined> => {
+  const [player, [todaysGames, todaysWins]] = await Promise.all([
+    playerDocument(playerAddress),
+    playsToday(playerAddress)
+  ])
+  if (!player) {
+    return {
+      level: 0,
+      nextLevelIn: 2,
+      wins: 0,
+      levelupWins: 0,
+      todaysGames,
+      todaysWins,
+      maxPerDay: MAX_LEVELING_GAMES_PER_DAY,
+    }
+  }
+  console.log("player", player, todaysGames, todaysWins)
+  const currentWins = player.levelup_wins
+  const currentLevel = Math.floor(Math.log(currentWins + 1));
+
+  // Calculate the number of wins required for the next level
+  const nextLevelWins = Math.ceil(Math.exp(currentLevel + 1)) - 1;
+
+  // Calculate the remaining wins needed for the next level
+  const remainingWins = nextLevelWins - currentWins;
+
+  return {
+    level: currentLevel,
+    nextLevelIn: remainingWins,
+    wins: player.wins,
+    levelupWins: currentWins,
+    todaysGames,
+    todaysWins,
+    maxPerDay: MAX_LEVELING_GAMES_PER_DAY,
+  };
+}
+
+const addressToDocId = (address: string): string => {
+  if (address.startsWith("0x")) {
+    return ID.custom(address.slice(2, 38).toLowerCase())
+  }
+  return ID.custom(address.slice(0, 36).toLowerCase())
+}
+
+const playerDocument = async (player: string): Promise<PlayerDoc|undefined> => {
+  const databases = new Databases(client);
+  try {
+    const doc = await databases.getDocument<PlayerDoc>(DATABASE_ID, PLAYER_COLLECTION, addressToDocId(player))
+    console.log("doc: ", doc)
+    return doc
+  } catch (err: any) {
+    if (err.code === 404) {
+      return undefined
+    }
+    throw err
+  }
+}
+
+export const usePlayerDetails = (playerAddress?: string) => {
+  return useQuery(["player-metagame", playerAddress], () => {
+    return playerDetail(playerAddress!)
+  }, {
+    enabled: !!playerAddress,
+  })
+}
--- empire-gambit/src/pages/game.tsx
+++ empire-gambit/src/pages/game.tsx
@@ -1,46 +1,56 @@
 import Layout from "@/components/Layout"
-import { Button, VStack } from "@chakra-ui/react"
+import { Button, Heading, VStack } from "@chakra-ui/react"
 import { NextPage } from "next"
 import Link from "next/link"
 import { useMemo } from "react"
 import { useAccount } from "wagmi"
 import { useUser } from "@/hooks/useUser"
 import { useSafeFromUser } from "@/hooks/useSafe"
+import { usePlayerDetails } from "@/hooks/usePlayerDetails"
 
 const GamePage: NextPage = () => {
-  const { data:user } = useUser()
+  const { data: user } = useUser()
   const { data: safeAddress } = useSafeFromUser()
+  const { data: playerDetails } = usePlayerDetails(safeAddress)
 
-  const { animationUrl:avatar, name:username } = user?.profile || {}
-  
+  const { animationUrl: avatar, name: username } = user?.profile || {}
+
   const gameParam = useMemo(() => {
-    return Buffer.from(JSON.stringify({id: safeAddress, name: username, avatar, numberOfHumans: 2 })).toString("base64")
+    return Buffer.from(JSON.stringify({ id: safeAddress, name: username, avatar, numberOfHumans: 2 })).toString("base64")
   }, [safeAddress, username, avatar])
 
   const aiGameParam = useMemo(() => {
-    return Buffer.from(JSON.stringify({numberOfHumans: 1, numberOfAi: 1, id: safeAddress, name: username, avatar })).toString("base64")
+    return Buffer.from(JSON.stringify({ numberOfHumans: 1, numberOfAi: 1, id: safeAddress, name: username, avatar })).toString("base64")
   }, [safeAddress, username, avatar])
 
   const tutorialParam = useMemo(() => {
-    return Buffer.from(JSON.stringify({roomType: "TutorialRoom", numberOfHumans: 1, numberOfAi: 1, id: safeAddress, name: username, avatar })).toString("base64")
+    return Buffer.from(JSON.stringify({ roomType: "TutorialRoom", numberOfHumans: 1, numberOfAi: 1, id: safeAddress, name: username, avatar })).toString("base64")
   }, [safeAddress, username, avatar])
 
   return (
     <Layout>
       {avatar && username && (
         <VStack spacing={10}>
+          {playerDetails && (
+            <VStack>
+              <Heading size="lg">Level: {playerDetails.level}</Heading>
+              <Heading size="lg">Wins until next level: {playerDetails.nextLevelIn}</Heading>
+              <Heading size="lg">Record for today: {playerDetails.wins} / {playerDetails.todaysGames}</Heading>
+              <Heading size="lg">Attempts remaining today: {Math.max(playerDetails.maxPerDay - playerDetails.todaysGames, 0)}</Heading>
+            </VStack>
+          )}
           <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${gameParam}`} target="_blank">
-              <Button variant={"secondary"}>Play against people</Button>
+            <Button variant={"secondary"}>Play against people</Button>
           </Link>
 
           <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${aiGameParam}`} target="_blank">
-              <Button variant={"primary"}>Play Against AI</Button>
+            <Button variant={"primary"}>Play Against AI</Button>
           </Link>
 
           <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${tutorialParam}`} target="_blank">
-              <Button variant={"secondary"}>Practice Room</Button>
+            <Button variant={"secondary"}>Practice Room</Button>
           </Link>
-          
+
         </VStack>
       )}
     </Layout>
