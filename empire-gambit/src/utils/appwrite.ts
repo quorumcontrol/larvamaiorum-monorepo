8a55cb839f031dd695e214925d75e82b8745301b
Topper Bowers <topper@toppingdesign.com>
2023-04-04 22:06:39 +0200

    appwrite for the game page (#40)

---
+++ empire-gambit/src/utils/appwrite.ts
@@ -0,0 +1,9 @@
+import { Client, Account, ID } from 'appwrite';
+
+const client = new Client();
+
+client
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject('642adf7622458e6f1519')
+
+export default client
