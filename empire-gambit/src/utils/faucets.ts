97f137e5e11f8489fa7f65c522408cfdbb9250f9
Topper Bowers <topper@toppingdesign.com>
2023-05-15 08:56:27 +0200

    better faucet

--- empire-gambit/src/utils/faucets.ts
+++ empire-gambit/src/utils/faucets.ts
@@ -1,22 +1,33 @@
 import { PoWSecure__factory } from "@/contract-types"
-import { Signer } from "ethers"
+import { Signer, utils } from "ethers"
 import { fetchAddresses } from "./fetchAddresses"
 import { AnonymousPoW } from "@skaleproject/pow-ethers"
 import { JsonRpcProvider } from "@ethersproject/providers"
 
 export const powFauct = (network:string) => {
   return async (address: string, signer?:Signer) => {
+    if (!signer) {
+      console.error("no signer for faucet")
+      return
+    }
+    
+    if ((await signer.getBalance()) > utils.parseEther("0.00001")) {
+      console.log("already funded")
+      return
+    }  
+
     const addresses = fetchAddresses(network)
     const pow = PoWSecure__factory.connect(addresses.PoWSecure.address, signer!)
     const populatedTx = await pow.populateTransaction.pay(address)
 
-    const powInstance = new AnonymousPoW({ rpcUrl: (signer?.provider! as JsonRpcProvider).connection.url })
+    const powInstance = new AnonymousPoW({ rpcUrl: (signer.provider! as JsonRpcProvider).connection.url })
     const tx = await powInstance.send({
       to: pow.address,
       data: populatedTx.data!,
     })
     console.log("funding tx: ", tx.hash)
     await tx.wait()
+    console.log("funding complete")
   }
 }
 
