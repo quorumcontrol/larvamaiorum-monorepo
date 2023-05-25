9edd95eaf123fa0e948dbfaa3546d7e409fc8dd6
Topper Bowers <topper@toppingdesign.com>
2023-05-26 00:06:57 +0200

    show your avatar on the game page

--- empire-gambit/src/pages/game.tsx
+++ empire-gambit/src/pages/game.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/Layout"
-import { Button, Heading, Spinner, VStack } from "@chakra-ui/react"
+import { Box, Button, Center, HStack, Heading, Spinner, VStack } from "@chakra-ui/react"
 import { NextPage } from "next"
 import Link from "next/link"
 import { useEffect, useMemo } from "react"
@@ -7,6 +7,7 @@ import { useUser } from "@/hooks/useUser"
 import { useSafeFromUser } from "@/hooks/useSafe"
 import { usePlayerDetails } from "@/hooks/usePlayerDetails"
 import Router from "next/router"
+import { Avatar } from '@readyplayerme/visage';
 import useIsClientSide from "@/hooks/useIsClientSide"
 
 const GamePage: NextPage = () => {
@@ -34,7 +35,7 @@ const GamePage: NextPage = () => {
       return
     }
     Router.push("/profile/edit/start")
-  }, [user]) 
+  }, [user])
 
   if (!isClient || !user) {
     return (
@@ -50,28 +51,41 @@ const GamePage: NextPage = () => {
   return (
     <Layout>
       {avatar && username && (
-        <VStack spacing={10}>
-          {playerDetails && (
-            <VStack>
-              <Heading size="lg">Level: {playerDetails.level}</Heading>
-              <Heading size="lg">Wins until next level: {playerDetails.nextLevelIn}</Heading>
-              <Heading size="lg">Record for today: {playerDetails.todaysWins} / {playerDetails.todaysGames}</Heading>
-              <Heading size="lg">Attempts remaining today: {Math.max(playerDetails.maxPerDay - playerDetails.todaysGames, 0)}</Heading>
+        <Center>
+          <HStack>
+            <Box height="600px">
+              <Avatar
+                modelSrc={avatar}
+                animationSrc="/standingIdle.glb"
+                halfBody={false}
+                cameraTarget={1.65}
+                cameraInitialDistance={2.5}
+                />
+            </Box>
+            <VStack spacing={4} alignItems="left">
+              {playerDetails && (
+                <VStack alignItems="left" spacing="4">
+                  <Heading size="md">Level: {playerDetails.level}</Heading>
+                  <Heading size="md">Wins until next level: {playerDetails.nextLevelIn}</Heading>
+                  <Heading size="md">Record for today: {playerDetails.todaysWins} / {playerDetails.todaysGames}</Heading>
+                  <Heading size="md">Attempts remaining today: {Math.max(playerDetails.maxPerDay - playerDetails.todaysGames, 0)}</Heading>
+                </VStack>
+              )}
+              {/* <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${gameParam}`} target="_blank">
+                <Button variant={"secondary"}>Play against people</Button>
+              </Link> */}
+
+              <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${aiGameParam}`} target="_blank">
+                <Button variant={"primary"}>Play Against AI</Button>
+              </Link>
+
+              <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${tutorialParam}`} target="_blank">
+                <Button variant={"secondary"}>Practice Room</Button>
+              </Link>
+
             </VStack>
-          )}
-          <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${gameParam}`} target="_blank">
-            <Button variant={"secondary"}>Play against people</Button>
-          </Link>
-
-          <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${aiGameParam}`} target="_blank">
-            <Button variant={"primary"}>Play Against AI</Button>
-          </Link>
-
-          <Link href={`https://playcanv.as/p/SP3UNx7J/?arena=true&m=${tutorialParam}`} target="_blank">
-            <Button variant={"secondary"}>Practice Room</Button>
-          </Link>
-
-        </VStack>
+          </HStack>
+        </Center>
       )}
     </Layout>
   )
