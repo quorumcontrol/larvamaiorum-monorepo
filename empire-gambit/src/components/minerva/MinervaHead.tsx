bb48283a06b1f3c8503b831e4b5b6d78a48d5199
Topper Bowers <topper@toppingdesign.com>
2023-06-05 10:51:58 +0200

    remove http for https

--- empire-gambit/src/components/minerva/MinervaHead.tsx
+++ empire-gambit/src/components/minerva/MinervaHead.tsx
@@ -10,11 +10,11 @@ const MinervaHead: React.FC = () => {
 
       <meta property="og:title" content="Discover Minerva: Your AI Fortune Teller | Empire Gambit" />
       <meta property="og:description" content="Join Minerva in Empire Gambit for a journey into the world of astrology and tarot. Enjoy a free personalized reading every day and a chance to earn unique digital collectibles." />
-      <meta property="og:image" content="http://empiregambit.com/minerva_landing_social.png" />
+      <meta property="og:image" content="https://empiregambit.com/minerva_landing_social.png" />
 
       <meta name="twitter:title" content="Discover Minerva: Your AI Fortune Teller | Empire Gambit" />
       <meta name="twitter:description" content="Experience the future of fortune telling with Minerva in Empire Gambit. Get a free personalized astrology or tarot reading daily and a chance to win digital collectibles." />
-      <meta name="twitter:image" content="http://empiregambit.com/minerva_landing_social.png" />
+      <meta name="twitter:image" content="https://empiregambit.com/minerva_landing_social.png" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   )
